a9ffe1728fd7f65946383ffa58e5c119e02ccd39
Kens <kensbotz@gmail.com>
2023-11-13 19:49:11 +0700

    add nexo

---
+++ system/nexo.js
@@ -0,0 +1,6 @@
+const { ApiNexon } = new(require('nexonwb'))
+const axios = require('axios'),
+   cheerio = require('cheerio'),
+   FormData = require('form-data'),
+   fetch = require('node-fetch'),
+   fs = require('fs)
