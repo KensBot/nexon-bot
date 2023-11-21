c9376c7f8e5828f7df7bc3ce7981a2091134bdaf
Kens <kensbotz@gmail.com>
2023-11-22 01:24:35 +0700

    fix server to index

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const axios = require('axios')
+const chalk = require('chalk')
 const express = require('express')
-const server = express()
-const PORT = process.env.PORT || 3000
-server.set('json spaces', 2)
-server.get('/', async (req, res) => {
-   res.json({
-      online: true,
-      msg: `Server running with port ${PORT}`
-   })
-})
+const app = express()
+const http = require('http')
+const env = require('./system/settings.json')
+const PORT = process.env.PORT || 8080
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+let i = 0
 
-server.listen(PORT, () => console.log(`Server running with port ${PORT}!`))
+const runServer = async () => {
+   app.get('/', (req, res) => res.send('Server Active!'))
+   const server = http.createServer(app)
+   server.listen(PORT, () => console.log(chalk.yellowBright.bold('Connected to server --', PORT)))
+   while (true) {
+      i++
+      try {
+         // add your server link on config,json for run 24×7hours. If you are deploying on replit
+         let response = await axios(env.replit_url || 'https://google.com')
+         if (env.replit_url) console.log(chalk.yellowBright.bold('Server wake-up! --', response.status))
+         await sleep(30_000)
+      } catch {}
+   }
+}
+
+runServer().then(() => runServer())
